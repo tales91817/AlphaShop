0462ef71bc3b2607cb86627d6f82bfbf947abfe3
tales91817 <tales91817@hotmail.com>
2023-05-02 22:35:19 +0800

    update CartInfo

--- src/components/Main/Cart-info/CartInformation.jsx
+++ src/components/Main/Cart-info/CartInformation.jsx
@@ -6,13 +6,13 @@ function CartContent() {
   // const content = [
   //   {
   //     productName: "破壞補丁修身牛仔褲",
-  //     productImg: Product01,
+  //     productImg: <Product01 />,
   //     price: '$3,999',
   //     count: 1,
   //   },
   //   {
   //     productName: "刷色直筒牛仔褲",
-  //     productImg: Product02,
+  //     productImg: <Product02 />,
   //     price: '$1,299',
   //     count: 1,
   //   },
